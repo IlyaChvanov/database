38cca447c4a568b3be7cd28366516bbea7735a68
ilya <ivchvanov@edu.hse.ru>
2024-12-08 12:14:42 +0300

    added Add Student

--- src/main/java/org/example/database/model/DataBase.java
+++ src/main/java/org/example/database/model/DataBase.java
@@ -8,23 +8,32 @@ import java.nio.file.Paths;
 @Data
 public class DataBase {
     private File dbFile;
+    private static DataBase instance = null;
     private DataBase() {
-        try {
-            this.dbFile = Paths.get("src/main/resources", "maindb").toFile();
-            FileWriter fileOutputStream = new FileWriter(dbFile);
-            fileOutputStream.write("Hello World");
-            fileOutputStream.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        this.dbFile = Paths.get("src/main/resources", "maindb").toFile();
     }
+
     static public DataBase getInstance() {
         if (instance == null) {
             instance = new DataBase();
         }
         return instance;
     }
-    private static DataBase instance = null;
+    public boolean addStudent(Student student) {
+        System.out.println("Adding student: " + student.toString());
+        try(FileWriter fileOutputStream = new FileWriter(dbFile)) {
+            StringBuilder studentData = new StringBuilder();
+            studentData.append(student.getId()).append(" ")
+                    .append(student.getName()).append(" ")
+                    .append(student.getCourse()).append(" ")
+                    .append(student.getDateOfBirth()).append(" ")
+                    .append(student.getMajor()).append(" ");
+            System.out.println(studentData);
+            fileOutputStream.write(studentData.toString());
+        } catch (IOException e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
--- src/test/java/org/example/database/DataBaseTest.java
+++ src/test/java/org/example/database/DataBaseTest.java
@@ -2,20 +2,33 @@ package org.example.database;
 
 
 import org.example.database.model.DataBase;
+import org.example.database.model.Major;
+import org.example.database.model.Student;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DataBaseTest {
+    DataBase dataBase = DataBase.getInstance();
+    @BeforeEach
+    public void setUp() {
+        try (FileWriter fileWriter = new FileWriter(dataBase.getDbFile(), false)) {
+            fileWriter.write("");  // Очистить файл перед каждым тестом
+            System.out.println("File has been cleaned up before the test.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @Test
-    void testClassConnectedToFile() throws FileNotFoundException {
-        DataBase dataBase = DataBase.getInstance();
-        FileReader fileReader = new FileReader(dataBase.getDbFile());
+    void testClassConnectedToFile() throws IOException {
         StringBuilder fileContent = new StringBuilder();
+        FileWriter fileWriter = new FileWriter(dataBase.getDbFile());
+        fileWriter.write("Hello World");
+        fileWriter.close();
         try (BufferedReader reader = new BufferedReader(new FileReader(dataBase.getDbFile()))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -26,5 +39,22 @@ public class DataBaseTest {
         }
 
         assertEquals("Hello World", fileContent.toString());
+    }
+
+    @Test
+    void testAddingStudent() {
+        dataBase.addStudent(new Student(1, "vasya", 1, LocalDate.of(2000, 5, 22), Major.ART));
+
+        StringBuilder fileContent = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(dataBase.getDbFile()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                fileContent.append(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals("1 vasya 1 2000-05-22 ART ", fileContent.toString());
     }
 }
